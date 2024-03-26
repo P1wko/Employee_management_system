5b7c5fe68255659816a01f5ae57de49bde3e7ec6
P1wko <miminios20611@gmail.com>
2024-03-26 17:44:26 +0100

    Validating loging credentials

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -14,6 +14,7 @@ using System.Windows.Navigation;
 using System.Windows.Shapes;
 using MySql.Data.MySqlClient;
 using System.Data;
+using System.Reflection;
 
 namespace Platformy_Projekt
 {
@@ -35,7 +36,7 @@ namespace Platformy_Projekt
             contentGrid.Children.Add(usersGrid);
             usersGrid.Margin = new Thickness(0, 0, 0, 0);
             usersGrid.GridLinesVisibility = DataGridGridLinesVisibility.None;
-            usersGrid.IsReadOnly = true;
+            //usersGrid.IsReadOnly = true;
             usersGrid.AutoGenerateColumns = true;
 
             MySqlConnection connection = new MySqlConnection(connectionString);
@@ -50,21 +51,86 @@ namespace Platformy_Projekt
 
                 MySqlDataAdapter dataAdapter = new MySqlDataAdapter(command);
                 DataTable dt = new DataTable("users");
-
                 dataAdapter.Fill(dt);
 
                 dt.Columns[0].ColumnName = "ID";
                 dt.Columns[1].ColumnName = "Name";
                 dt.Columns[2].ColumnName = "Surname";
+                //dt.Columns[2].ColumnName = "Department";
+                //dt.Columns[3].ColumnName = "E-mail";
                 dt.Columns[3].ColumnName = "Salary";
+                //dt.Columns[3].ColumnName = "Actions";
 
                 usersGrid.ItemsSource = dt.DefaultView;
+
+                DataGridComboBoxColumn actionsColumn = new DataGridComboBoxColumn();
+                actionsColumn.Header = "Actions";
+                actionsColumn.ItemsSource = getActionsColumnItems();
+
+                //actionsColumn.SelectedValueBinding = new Binding("ID");
+
+                usersGrid.Columns.Add(actionsColumn);
                 
                 dataAdapter.Update(dt);
                 connection.Close();
             } catch (Exception ex)
             {
                 MessageBox.Show(ex.Message);
+            }
+        }
+
+        private List<Button> getActionsColumnItems()
+        {
+            List<Button> items = new List<Button>();
+            Button sendMsg = new Button();
+            sendMsg.Content = "Send Message";
+            items.Add(sendMsg);
+
+            Button edit = new Button();
+            edit.Content = "Edit";
+            items.Add(edit);
+
+            Button delete = new Button();
+            delete.Content = "Delete";
+            items.Add(delete);
+
+            return items;
+        }
+
+        private void loginBtn_Click(object sender, RoutedEventArgs e)
+        {
+            MySqlConnection connection = new MySqlConnection(connectionString);
+            try
+            {
+                connection.Open();
+                string lgn = login.Text;
+                string pswd = passwd.Text;
+                string query = "SELECT * FROM users WHERE login='" + lgn + "';";
+                MySqlCommand command = new MySqlCommand(query, connection);
+                command.ExecuteNonQuery();
+                MySqlDataAdapter dataAdapter = new MySqlDataAdapter(command);
+                DataTable dt = new DataTable("siema");
+                dataAdapter.Fill(dt);
+
+                //DataGrid loginy = new DataGrid();
+                //contentGrid.Children.Add(loginy);
+                //loginy.AutoGenerateColumns = true;
+                //loginy.ItemsSource = dt.DefaultView;
+
+                if (pswd == dt.Rows[0]["password"].ToString())
+                {
+                    employeesBtn.IsEnabled = true;
+                    tasksBtn.IsEnabled = true;
+                    messagesBtn.IsEnabled = true;
+                    logoutBtn.IsEnabled = true;
+                    timerBtn.IsEnabled = true;
+                    MessageBox.Show("dobrze");
+                }
+
+
+            } catch (Exception ex)
+            {
+                MessageBox.Show("Błędne dane logowania");
             }
         }
     }
